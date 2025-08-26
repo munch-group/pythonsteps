bdbc4ffe71dfc01b2d8dd9f673867db01808f7a3
Kasper Munch <kaspermunch@birc.au.dk>
2025-08-27 01:40:53 +0200

    patch  version bump

--- src/pysteps/text_gui.py
+++ src/pysteps/text_gui.py
@@ -803,8 +803,7 @@ class STEPS(Screen):
 class STEPSApp(App):
     CSS_PATH = "text_gui.css"
     dark = False
-    # SCREENS = {"steps": STEPS()}
-    SCREENS = {"steps": STEPS}
+    SCREENS = {"steps": STEPS()}
     BINDINGS = [("escape", "push_screen('steps')", "STEPS")]
 
     def on_mount(self) -> None:
